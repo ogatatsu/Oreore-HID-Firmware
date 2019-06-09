371a7757440ddd9aa9eeb541586262889d8e7a49
ogatatsu <ogwrtty@gmail.com>
2019-06-09 19:11:41 +0900

    frameworkにパッチを当てるコードを追加

---
+++ apply_patches.py
@@ -0,0 +1,18 @@
+from os.path import join, isfile
+import sys
+
+Import("env")
+
+framework_dir = env.PioPlatform().get_package_dir("framework-arduinoadafruitnrf52")
+patchflag_file = join(framework_dir, ".patching-done")
+patch_file = "framework-arduinoadafruitnrf52.patch"
+
+if not isfile(patchflag_file):
+
+    env.Execute("%s patch.py -d %s %s" % (sys.executable, framework_dir, patch_file))
+
+    def _touch(path):
+        with open(path, "w") as fp:
+            fp.write("")
+
+    env.Execute(lambda *args, **kwargs: _touch(patchflag_file))
