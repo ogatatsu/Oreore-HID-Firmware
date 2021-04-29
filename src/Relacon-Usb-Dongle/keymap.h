f53f26d57360a8741c47d9d02d1a3ca0fcc3d1e1
ogatatsu <ogwrtty@gmail.com>
2021-04-29 22:50:36 +0900

    HID-Playground-Libをアップデート、キーマップを変更

--- src/Relacon-Usb-Dongle/keymap.h
+++ src/Relacon-Usb-Dongle/keymap.h
@@ -8,12 +8,13 @@ using namespace hidpg;
 Key keymap[] = {
   { 0, MS_CLK(LeftButton) },
   { 1, TD({ { MS_CLK(RightButton), MS_CLK(RightButton) },
-            { TAP(MS_CLK(RightButton), 2), TRC(0) } }) },
+            { TAP(MS_CLK(RightButton), 2), TRC(0) } },
+          true) },
   { 2, MS_CLK(MiddleButton) },
   { 3, MS_CLK(BackwardButton) },
   { 4, MS_CLK(ForwardButton) },
-  { 10, CC(Next) },
-  { 11, CC(Prev) },
+  { 10, CC(NextTrack) },
+  { 11, CC(PrevTrack) },
   { 12, CC(PlayPause) },
   { 13, CC(VolumeUp) },
   { 14, CC(VolumeDown) },
@@ -24,5 +25,5 @@ Encoder encoderMap[] = {
 };
 
 Track trackMap[] = {
-  { 0, 25, MS_SCR(1, 0), MS_SCR(-1, 0) }
+  { 0, 20, AngleSnap::Disable, MS_SCR(1, 0), MS_SCR(-1, 0), MS_SCR(0, -1), MS_SCR(0, 1) }
 };
--- src/TrackPoint-Keyboard-II-Usb-Dongle/keymap.h
+++ src/TrackPoint-Keyboard-II-Usb-Dongle/keymap.h
@@ -1,9 +1,14 @@
 
+#include "CommandTapper.h"
 #include "HidCore.h"
 #include "HidEngine.h"
 #include "KC.h"
 
 using namespace hidpg;
+
+//------------------------------------------------------------------+
+// CustomCommand
+//------------------------------------------------------------------+
 
 class ScrollOrTap : public Command
 {
@@ -15,23 +20,23 @@ public:
 
   uint8_t onRelease() override
   {
+    xSemaphoreTake(_mutex, portMAX_DELAY);
     if (_isScroll == false)
     {
-      _tap_command->press();
-      _tap_command->release();
+      CommandTapper.tap(_tap_command);
     }
     else
     {
-      xSemaphoreTake(_mutex, portMAX_DELAY);
       _isScroll = false;
-      xSemaphoreGive(_mutex);
     }
+    xSemaphoreGive(_mutex);
+
     return 1;
   }
 
   static void init()
   {
-    _mutex = xSemaphoreCreateMutex();
+    _mutex = xSemaphoreCreateMutexStatic(&_mutex_buffer);
   }
 
   static void notifyScroll()
@@ -44,47 +49,60 @@ public:
 private:
   Command *_tap_command;
   static SemaphoreHandle_t _mutex;
+  static StaticSemaphore_t _mutex_buffer;
   static bool _isScroll;
 };
 
 bool ScrollOrTap::_isScroll = false;
 SemaphoreHandle_t ScrollOrTap::_mutex = nullptr;
+StaticSemaphore_t ScrollOrTap::_mutex_buffer;
+
+ScrollOrTap ScrollOrMiddleButton(MS_CLK(MiddleButton));
+
+//------------------------------------------------------------------+
+// Keymap
+//------------------------------------------------------------------+
 
 Key keymap[] = {
   { 0 /*  LeftButton    */, MS_CLK(LeftButton) },
   { 1 /*  RightButton   */, MS_CLK(RightButton) },
-  { 2 /*  MiddleButton  */, new ScrollOrTap(MS_CLK(MiddleButton)) },
+  { 2 /*  MiddleButton  */, &ScrollOrMiddleButton },
 
-  { 4 /*  a             */, LY1({ NK(A), NK(F21) }) },
+  { 4 /*  a             */, NK(A) },
   { 5 /*  b             */, NK(B) },
-  { 6 /*  c             */, LY1({ NK(C), NK(KeypadEqual) }) },
-  { 7 /*  d             */, LY1({ NK(D), NK(F23) }) },
-  { 8 /*  e             */, LY1({ NK(E), NK(F19) }) },
-  { 9 /*  f             */, LY1({ NK(F), NK(F24) }) },
-  { 10 /* g             */, LY1({ NK(G), NK(Pause) }) },
-  { 11 /* h             */, LY1({ NK(H), NK(ArrowLeft) }) },
-  { 12 /* i             */, LY1({ NK(I), NK(Home) }) },
-  { 13 /* j             */, LY1({ NK(J), NK(ArrowDown) }) },
-  { 14 /* k             */, LY1({ NK(K), NK(ArrowUp) }) },
-  { 15 /* l             */, LY1({ NK(L), NK(ArrowRight) }) },
-  { 16 /* m             */, LY1({ NK(M), NK(Application) }) },
+  { 6 /*  c             */, NK(C) },
+  { 7 /*  d             */, NK(D) },
+  { 8 /*  e             */, NK(E) },
+  { 9 /*  f             */, NK(F) },
+  { 10 /* g             */, NK(G) },
+  { 11 /* h             */, LY1({ LY({ NK(H), NK(ArrowLeft), NK(Backspace) }),
+                                  CC(PrevTrack) }) },
+  { 12 /* i             */, LY({ NK(I), NK(Home) }) },
+  { 13 /* j             */, LY1({ LY({ NK(J), NK(ArrowDown) }),
+                                  CC(VolumeDown) }) },
+  { 14 /* k             */, LY1({ LY({ NK(K), NK(ArrowUp) }),
+                                  CC(VolumeUp) }) },
+  { 15 /* l             */, LY1({ LY({ NK(L), NK(ArrowRight), NK(Delete) }),
+                                  CC(NextTrack) }) },
+  { 16 /* m             */, LY1({ LY({ NK(M), NK(Application) }),
+                                  CC(Mute) }) },
   { 17 /* n             */, NK(N) },
-  { 18 /* o             */, LY1({ NK(O), NK(End) }) },
-  { 19 /* p             */, LY1({ NK(P), NK(PageUp) }) },
-  { 20 /* q             */, LY1({ NK(Q), NK(F17) }) },
-  { 21 /* r             */, LY1({ NK(R), NK(F20) }) },
-  { 22 /* s             */, LY1({ NK(S), NK(F22) }) },
+  { 18 /* o             */, LY({ NK(O), NK(End) }) },
+  { 19 /* p             */, LY({ NK(P), NK(PageUp) }) },
+  { 20 /* q             */, NK(Q) },
+  { 21 /* r             */, NK(R) },
+  { 22 /* s             */, NK(S) },
   { 23 /* t             */, NK(T) },
   { 24 /* u             */, NK(U) },
-  { 25 /* v             */, LY1({ NK(V), NK(ScrollLock) }) },
-  { 26 /* w             */, LY1({ NK(W), NK(F18) }) },
-  { 27 /* x             */, LY1({ NK(X), NK(KeypadComma) }) },
+  { 25 /* v             */, NK(V) },
+  { 26 /* w             */, NK(W) },
+  { 27 /* x             */, NK(X) },
   { 28 /* y             */, NK(Y) },
-  { 29 /* z             */, LY1({ NK(Z), NK(Int6) }) },
-  { 30 /* 1             */, LY1({ NK(_1), NK(F13) }) },
-  { 31 /* 2             */, LY1({ NK(_2), NK(F14) }) },
-  { 32 /* 3             */, LY1({ NK(_3), NK(F15) }) },
-  { 33 /* 4             */, LY1({ NK(_4), NK(F16) }) },
+  { 29 /* z             */, NK(Z) },
+  { 30 /* 1             */, NK(_1) },
+  { 31 /* 2             */, NK(_2) },
+  { 32 /* 3             */, NK(_3) },
+  { 33 /* 4             */, NK(_4) },
   { 34 /* 5             */, NK(_5) },
   { 35 /* 6             */, NK(_6) },
   { 36 /* 7             */, NK(_7) },
@@ -92,25 +110,23 @@ Key keymap[] = {
   { 38 /* 9             */, NK(_9) },
   { 39 /* 0             */, NK(_0) },
   { 40 /* Enter         */, NK(Enter) },
-  { 41 /* Escape        */, NK(Escape) },
+  { 41 /* Escape        */, MLT({ TRC(2), RD_CLK() }) },
   { 42 /* Backspace     */, NK(Backspace) },
   { 43 /* Tab           */, NK(Tab) },
-  { 44 /* Space         */, NK(Space) },
+  { 44 /* Space         */, LY1({ NK(Space), CC(PlayPause) }) },
   { 45 /* -             */, NK(Minus) },
   { 46 /* ^             */, NK(Equal) },
   { 47 /* @             */, NK(BracketLeft) },
   { 48 /* [             */, NK(BracketRight) },
   { 50 /* ]             */, NK(NonUsNumberSign) },
-  { 51 /* ;             */, LY1({ NK(Semicolon), NK(PageDown) }) },
+  { 51 /* ;             */, LY({ NK(Semicolon), NK(PageDown) }) },
   { 52 /* :             */, NK(Quote) },
-  { 53 /* Han/Zen       */, NK(Escape) },
-  { 54 /* ,             */, NK(Comma) },
-  { 55 /* .             */, NK(Period) },
+  { 53 /* Han/Zen       */, TRC(2) },
+  { 54 /* ,             */, LY1({ NK(Comma), CC(Rewind) }) },
+  { 55 /* .             */, LY1({ NK(Period), CC(FastForward) }) },
   { 56 /* /             */, NK(Slash) },
-  { 57 /* CapsLock      */, LY2({ TD({ { MO(Ctrl), MO(Ctrl) },
-                                       { TAP(MO(Ctrl), 2), MO(Ctrl + Shift) },
-                                       { TAP(MO(Ctrl), 3), MO(Ctrl + Shift + Alt) } }),
-                                  MO(Ctrl) }) },
+  { 57 /* CapsLock      */, TD({ { NK(Escape), MO(Ctrl) },
+                                 { TAP(MO(Ctrl), 2), MO(Ctrl + Shift) } }) },
   { 58 /* F1            */, NK(F1) },
   { 59 /* F2            */, NK(F2) },
   { 60 /* F3            */, NK(F3) },
@@ -123,12 +139,12 @@ Key keymap[] = {
   { 67 /* F10           */, NK(F10) },
   { 68 /* F11           */, NK(F11) },
   { 69 /* F12           */, NK(F12) },
-  { 70 /* PrtSc         */, NK(PrintScreen) },
-
+  { 70 /* PrtSc         */, TD({ { NK(PrintScreen), NK(PrintScreen) },
+                                 { CK(Gui + Shift, S), CK(Gui + Shift, S) } }) },
   { 73 /* Insert        */, NK(Insert) },
   { 74 /* Home          */, NK(Home) },
   { 75 /* PgUp          */, NK(PageUp) },
-  { 76 /* Delete        */, NK(Delete) },
+  { 76 /* Delete        */, LY({ NK(Delete), SC(SystemSleep) }) },
   { 77 /* End           */, NK(End) },
   { 78 /* PgDn          */, NK(PageDown) },
   { 79 /* ArrowRight    */, NK(ArrowRight) },
@@ -137,29 +153,96 @@ Key keymap[] = {
   { 82 /* ArrowUp       */, NK(ArrowUp) },
 
   { 135 /* \(yen)       */, NK(Int1) },
-  { 136 /* Kana         */, LY1({ NK(Backspace), NK(Delete) }) },
+  { 136 /* Kana         */, TD({ { CC(LaunchApp1), CC(LaunchApp1) },
+                                 { CC(LaunchApp2), CC(LaunchApp2) },
+                                 { CC(LaunchMail), CC(LaunchMail) },
+                                 { CC(LaunchMedia), CC(LaunchMedia) } }) },
   { 137 /* \(BackSlash) */, NK(Int3) },
-  { 138 /* Henkan       */, TD({ { MLT({ NK(Int4), NK(Lang1) }), SL1(1) },
-                                 { MS_CLK(ForwardButton), MS_CLK(ForwardButton) } }) },
-  { 139 /* Muhenkan     */, TD({ { MLT({ NK(Int5), NK(Lang2) }), SL1(1) },
-                                 { MS_CLK(BackwardButton), MS_CLK(BackwardButton) } }) },
+  { 138 /* Henkan       */, TD({ { MLT({ NK(Int4), NK(Lang1) }), MLT({ SL(1), TRC(0) }) },
+                                 { MS_CLK(ForwardButton), MLT({ SL(2), TRC(1) }) } },
+                               true) },
+  { 139 /* Muhenkan     */, TD({ { MLT({ NK(Int5), NK(Lang2) }), MLT({ SL(1), TRC(0) }) },
+                                 { MS_CLK(BackwardButton), MLT({ SL(2), TRC(1) }) } },
+                               true) },
 
-  { 200 /* LeftCtrl     */, NOP() },
-  { 201 /* LeftShift    */, LY2({ TD({ { MO(Shift), MO(Shift) },
-                                       { CK(Shift, CapsLock), CK(Shift, CapsLock) } }),
-                                  MO(Shift) }) },
+  { 200 /* LeftCtrl     */, MLT({ SL1(1), TRC(3) }) },
+  { 201 /* LeftShift    */, TD({ { MO(Shift), MO(Shift) },
+                                 { CK(Shift, CapsLock), CK(Shift, CapsLock) } }) },
   { 202 /* LeftAlt      */, TD({ { MO(Alt), MO(Alt) },
-                                 { TAP(MO(Alt), 2), MO(Alt + Shift) },
-                                 { TAP(MO(Alt), 3), MO(Alt + Ctrl) } }) },
+                                 { TAP(MO(Alt), 2), MO(Alt + Shift) } }) },
   { 203 /* LeftGui      */, MO(Gui) },
-  { 204 /* RightCtrl    */, TL2(1) },
+  { 204 /* RightCtrl    */, TD({ { MO(RightCtrl), MO(RightCtrl) },
+                                 { TAP(MO(RightCtrl), 2), MO(RightCtrl + Shift) } }) },
   { 205 /* RightShift   */, TD({ { MO(RightShift), MO(RightShift) },
                                  { CK(RightShift, CapsLock), CK(RightShift, CapsLock) } }) },
   { 206 /* RightAlt     */, TD({ { MO(RightAlt), MO(RightAlt) },
-                                 { MO(Alt + Shift), MO(Alt + Shift) },
-                                 { MO(Alt + Ctrl), MO(Alt + Ctrl) } }) },
+                                 { TAP(MO(RightAlt), 2), MO(RightAlt + Shift) } }) },
+
+  { 255 /* Dongle Button */, NOP() },
 };
 
 Encoder encoderMap[] = {
   { 0, MS_SCR(-1, 0), MS_SCR(1, 0) },
 };
+
+Track trackMap[] = {
+  { 0, 100, AngleSnap::Enable, _______, _______, NK(Backspace), NK(Delete) },
+  { 1, 100, AngleSnap::Enable, NK(ArrowUp), NK(ArrowDown), NK(ArrowLeft), NK(ArrowRight) },
+  { 2, 1, AngleSnap::Enable, RD_ROT(-1), RD_ROT(1), RD_ROT(-1), RD_ROT(1) },
+  { 3, 100, AngleSnap::Enable, CC(VolumeUp), CC(VolumeDown), _______, _______ }
+};
+
+SimulKey simulKeymap[] = {
+  { { 138, 139 }, MLT({ SL(2), TRC(1) }) }
+};
+
+void layer_change_state_callback(layer_bitmap_t prev_state, layer_bitmap_t state)
+{
+  static bool f13_state = false;
+
+  if (bitRead(prev_state, 2) == 0 && bitRead(state, 2) == 1) // Layer2がonになった
+  {
+    // F13が入力されている場合解除してからF14を入力
+    if (f13_state == true)
+    {
+      Hid.unsetKey(F13);
+      f13_state = false;
+    }
+    Hid.setKey(F14);
+    Hid.sendKeyReport(false);
+  }
+  else if (bitRead(prev_state, 2) == 1 && bitRead(state, 2) == 0) // Layer2がoffになった
+  {
+    // F14を解除
+    Hid.unsetKey(F14);
+
+    // Layer1がonだったらF13を入力
+    if (bitRead(state, 1) == 1)
+    {
+      Hid.setKey(F13);
+      f13_state = true;
+    }
+    Hid.sendKeyReport(false);
+  }
+
+  if (bitRead(prev_state, 1) == 0 && bitRead(state, 1) == 1) // Layer1がonになった
+  {
+    // Layer2がoffだったらF13を入力
+    if (bitRead(state, 2) == 0)
+    {
+      Hid.setKey(F13);
+      Hid.sendKeyReport(false);
+      f13_state = true;
+    }
+  }
+  else if (bitRead(prev_state, 1) == 1 && bitRead(state, 1) == 0) // Layer1がoffになった
+  {
+    // F13が入力されている場合解除する
+    if (f13_state == true)
+    {
+      Hid.unsetKey(F13);
+      Hid.sendKeyReport(false);
+      f13_state = false;
+    }
+  }
+}
